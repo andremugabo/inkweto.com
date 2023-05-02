79bf66f5125390c01283bfacef11e30b6ef90943
andremugabo <andremugabo@yahoo.fr>
2023-05-02 03:20:26 +0200

    product details page

---
+++ PAGES/PRODUCTS/viewDetailsProduct.php
@@ -0,0 +1,77 @@
+<div class="productDetails">
+    <div class="main_product_detail">
+        <div class="product_img">
+            <div class="image_p" style="background-image: url('ASSETS/TESTIMAGES/sub6.png');">
+
+            </div>
+            <div class="sub_image_p">
+                <div class="subimg_p" style="background-image: url('ASSETS/TESTIMAGES/sub4.jpg');"></div>
+                <div class="subimg_p" style="background-image: url('ASSETS/TESTIMAGES/sub2.jpg');"></div>
+                <div class="subimg_p" style="background-image: url('ASSETS/TESTIMAGES/sub3.jpg');"></div>
+                <div class="subimg_p" style="background-image: url('ASSETS/TESTIMAGES/sub1.jpg');"></div>
+                <div class="subimg_p" style="background-image: url('ASSETS/TESTIMAGES/sub5.jpg');"></div>
+            </div>
+        </div>
+        <div class="product_details">
+            <div class="product_mark">
+                <h3>Adidas</h3>
+            </div>
+            <div class="product_descption">
+                <p>Durable Performance Cricket Shoes with Dual Closure, Laces and Strap Cricket Shoes For Men.</p>
+            </div>
+            <div class="product_price">
+                <h4>20,000&nbsp;Frw</h4>
+                <span>25.000&nbsp;Frw</span>
+            </div>
+            <div class="rating_product">
+                <span style="font-size: 150%;color: #777">&starf;</span>
+				<span style="font-size: 150%;color: #777">&starf;</span>
+				<span style="font-size: 150%;color: #777">&starf;</span>
+				<span style="font-size: 150%;color: #777">&starf;</span>
+				<span style="font-size: 150%;color: #777">&starf;</span>
+				<span class="review-value">4.7</span>&nbsp;&nbsp;
+				<span class="reviews">1456 Reviews</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<span class="orders">2456 Orders</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<span class="orders"><b>Shop:</b><a href="#" style="color: gray;font-size: 12px;">Shop Name</a></span>
+            </div>
+            <div class="separator"></div>
+            <div class="product_color">
+                <div class="color_value">
+                    <h5>Color:Blue</h5>
+                </div>
+                <div class="color_img">
+                    <div class="color_img_value" style="background-color:blue"></div>
+                    <div class="color_img_value" style="background-color:black"></div>
+                    <div class="color_img_value" style="background-color:fuchsia"></div>
+                    <div class="color_img_value" style="background-color:pink"></div>
+                    <div class="color_img_value" style="background-color:green"></div>
+                    <div class="color_img_value" style="background-color:yellow"></div>
+                    <div class="color_img_value" style="background-color:chartreuse"></div>
+                    <div class="color_img_value" style="background-color:deeppink"></div>
+
+                    
+                </div>
+            </div>
+
+            <div class="product_size">
+                <div class="size_value">
+                    <h5>Size:45</h5>
+                </div>
+                <div class="size_img">
+                    <div class="size_img_value" >37</div>
+                    <div class="size_img_value" >38</div>
+                    <div class="size_img_value" >39</div>
+                    <div class="size_img_value" >40</div>
+                    <div class="size_img_value" >41</div>
+                    <div class="size_img_value" >42</div>
+                    <div class="size_img_value" >43</div>
+                    <div class="size_img_value" >44</div>
+                    <div class="size_img_value" >45</div>                    
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
